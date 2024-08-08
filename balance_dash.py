cf395744e58f3a43cf5c0167db3c6d76fb6cc87c
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-08 13:28:13 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import io
+from xlsxwriter import Workbook
 #import pyodbc as odbc
 
 
