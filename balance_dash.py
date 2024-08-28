bf6b48f7572a1917f13d1e437f2bea022a422e6c
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-28 16:09:00 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -67,6 +67,40 @@ def unit_stock_price_distribution(df):
     
     return fig
 
+def unit_stock_price_distribution1(df):
+    # Define price bins with a more scalable approach
+    min_price = 0
+    max_price = 80000000
+    # Define bin edges; these values can be adjusted as needed
+    bin_edges = [min_price + i*(max_price-min_price)/80 for i in range(81)]
+    bin_labels = [f'{int(bin_edges[i]):,}-{int(bin_edges[i+1]):,}' for i in range(len(bin_edges)-1)]
+    # Assign bin labels to each price
+    df['PriceRange'] = pd.cut(df['BasePrice'], bins=bin_edges, labels=bin_labels, include_lowest=True)
+    # Aggregate quantity sold within each price range
+    price_range_distribution = df.groupby('PriceRange').sum()[['Quantity']].reset_index()
+    # Create bar chart
+    fig = px.bar(price_range_distribution, x='PriceRange', y='Quantity', title='Distribution of Stocks Unit Prices and Quantity Sold',
+                 color_discrete_sequence=['gold'])
+    
+    return fig
+
+def unit_stock_price_distribution2(df):
+    # Define price bins with a more scalable approach
+    min_price = 80000000
+    max_price = 200000000
+    # Define bin edges; these values can be adjusted as needed
+    bin_edges = [min_price + i*(max_price-min_price)/80 for i in range(81)]
+    bin_labels = [f'{int(bin_edges[i]):,}-{int(bin_edges[i+1]):,}' for i in range(len(bin_edges)-1)]
+    # Assign bin labels to each price
+    df['PriceRange'] = pd.cut(df['BasePrice'], bins=bin_edges, labels=bin_labels, include_lowest=True)
+    # Aggregate quantity sold within each price range
+    price_range_distribution = df.groupby('PriceRange').sum()[['Quantity']].reset_index()
+    # Create bar chart
+    fig = px.bar(price_range_distribution, x='PriceRange', y='Quantity', title='Distribution of Stocks Unit Prices and Quantity Sold',
+                 color_discrete_sequence=['gold'])
+    
+    return fig
+
 
 def unit_order_price_distribution(df):
     # Define price bins with a more scalable approach
@@ -91,9 +125,9 @@ def unit_order_price_distribution(df):
 st.plotly_chart(unit_stock_price_distribution(df_stocks))
 col1, col2 = st.columns((5, 5))
 with col1: 
-    st.plotly_chart(unit_stock_price_distribution(df_stocks))
+    st.plotly_chart(unit_stock_price_distribution1(df_stocks))
 with col2:
-    st.plotly_chart(unit_stock_price_distribution(df_stocks))
+    st.plotly_chart(unit_stock_price_distribution2(df_stocks))
     
 st.plotly_chart(unit_order_price_distribution(df_orders))
 
