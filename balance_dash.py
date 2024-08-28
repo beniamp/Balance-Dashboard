3b74d1374fbdb7887fe979851c378aba9509e46b
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-28 17:53:51 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -3,10 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import altair as alt
-from PIL import Image
-from convertdate import persian
-from datetime import datetime, timedelta
+
 
 
 # Defining the Component of Connection String
@@ -51,76 +48,6 @@ with open('style.css') as f:
 df_stocks = pd.read_csv('Stocks.csv')
 df_orders = pd.read_csv('Orders.csv')
 
-
-# Clean up category data
-df_orders['Category'] = df_orders['Category'].replace('گوشی موبایل ', 'گوشی موبایل')
-categories = ['All Categories'] + df_orders['Category'].unique().tolist()
-
-# Formatting and cleaning date values
-df_orders['Date_Formatted'] = df_orders['Date_Formatted'].fillna('0000-00-00')
-df_orders = df_orders[df_orders['Date_Formatted'] != '0000-00-00']
-
-# Ensure date is a string format
-df_orders['Date_value'] = df_orders['Date_Formatted'].str.replace('-', '').astype(str)
-sorted_dates = sorted(df_orders['Date_Formatted'].unique())
-
-# Function to convert Persian date to Gregorian date
-def persian_to_gregorian(persian_date_str):
-    year, month, day = map(int, persian_date_str.split('-'))
-    gregorian_date = persian.to_gregorian(year, month, day)
-    return datetime(gregorian_date[0], gregorian_date[1], gregorian_date[2])
-    
-# Convert Persian dates to Gregorian
-df_orders['Gregorian_Date'] = df_orders['Date_Formatted'].apply(persian_to_gregorian)
-
-# Date range selection using calendar widget
-b1, b2 = st.columns(2)
-sorted_dates_gregorian = sorted(df_orders['Gregorian_Date'].unique())
-start_date, end_date = b1.date_input(
-    "Select Date Range",
-    value=[sorted_dates_gregorian[0], sorted_dates_gregorian[-1]],
-    min_value=sorted_dates_gregorian[0],
-    max_value=sorted_dates_gregorian[-1]
-)
-selected_category = b2.selectbox('Select Category', categories)
-
-# Convert Gregorian dates back to Persian format
-def gregorian_to_persian(gregorian_date):
-    persian_date = persian.from_gregorian(gregorian_date.year, gregorian_date.month, gregorian_date.day)
-    return f'{persian_date[0]:04}-{persian_date[1]:02}-{persian_date[2]:02}'
-
-# Convert the selected Gregorian dates back to Persian format
-start_date_persian = gregorian_to_persian(start_date)
-end_date_persian = gregorian_to_persian(end_date)
-
-# Calculate the number of days in the selected range
-num_days = (end_date - start_date).days + 1
-
-# Calculate the previous date range
-previous_start_date = start_date - timedelta(days=num_days)
-previous_end_date = end_date - timedelta(days=num_days)
-
-previous_start_date_persian = gregorian_to_persian(previous_start_date)
-previous_end_date_persian = gregorian_to_persian(previous_end_date)
-
-# Filter DataFrame by date and category
-filtered_df = df_orders[
-    (df_orders['Gregorian_Date'] >= start_date) &
-    (df_orders['Gregorian_Date'] <= end_date)
-]
-
-# Filter DataFrame by current and previous date ranges
-current_filtered_df = df_orders[(df_orders['Date_Formatted'] >= start_date_persian) & (df_orders['Date_Formatted'] <= end_date_persian)]
-previous_filtered_df = df_orders[(df_orders['Date_Formatted'] >= previous_start_date_persian) & (df_orders['Date_Formatted'] <= previous_end_date_persian)]
-
-# Apply category filter if necessary
-if selected_category != 'All Categories':
-    current_filtered_df = current_filtered_df[current_filtered_df['Category'] == selected_category]
-    previous_filtered_df = previous_filtered_df[previous_filtered_df['Category'] == selected_category]
-
-st.write(f'Domain of period time: {num_days}')
-st.write(f'Current period range: {start_date_persian} to {end_date_persian}')
-st.write(f'Previous period range: {previous_start_date_persian} to {previous_end_date_persian}')
 
 def unit_stock_price_distribution(df):
     # Define price bins with a more scalable approach
