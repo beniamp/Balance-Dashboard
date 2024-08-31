7563dd1719002f735819a16525a16fe5fdcb82c2
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-31 10:49:33 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import jdatetime
+from convertdate import persian
+
 
 
 # Defining the Component of Connection String
