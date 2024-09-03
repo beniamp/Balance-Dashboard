3eadca45aad6666c62adc626781f6ce2f79cc296
beniamp <102410523+beniamp@users.noreply.github.com>
2024-09-03 12:43:17 +0330

    Update balance_dash.py

--- balance_dash.py
+++ balance_dash.py
@@ -150,7 +150,7 @@ bin_labels = [f'{int(bin_edges[i]):,}-{int(bin_edges[i+1]):,}' for i in range(le
 df_joined['PriceRangeS'] = pd.cut(df_joined['BasePriceStock'], bins=bin_edges, labels=bin_labels, include_lowest=True)
 
 price_ranges = df_joined['PriceRangeS'].unique().sort_values()
-price_ranges = df_joined['PriceRangeS'].fillna()
+price_ranges = df_joined['PriceRangeS'].notna()
 
 start_range = st.selectbox('Starter', price_ranges)
 ending_range = st.selectbox('Ending', price_ranges, index=len(price_ranges) - 1)
